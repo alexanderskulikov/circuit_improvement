80b2dea375c3deff9eb1d2b7bfa5eb5bdd2f5cda
Alexander S. Kulikov <alexander.s.kulikov@gmail.com>
2021-01-23 19:05:06 +0300

    Replace floor with ceil to fix a bug

--- sandbox.py
+++ sandbox.py
@@ -4,16 +4,16 @@ from itertools import product
 from circuit_search_tests import verify_sum_circuit
 from functions import *
 from circuit_improvement import improve_circuit
-from math import log2
+from math import ceil, log2
 
 
 if __name__ == '__main__':
 
     def sum_n(x):
         assert all(a in (0, 1) for a in x)
-        s, l = sum(x), int(log2(len(x) + 1))
+        s, l = sum(x), ceil(log2(len(x) + 1))
         return [(s >> i) & 1 for i in range(l)]
 
-    circuit_finder = CircuitFinder(dimension=3, number_of_gates=5, function=sum_n)
+    circuit_finder = CircuitFinder(dimension=4, number_of_gates=9, function=sum_n)
     circuit = circuit_finder.solve_cnf_formula()
     print(circuit)
