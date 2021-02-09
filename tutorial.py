94156ad13e7ff8f60be61a6a1a8a7a8d82d6a23b
nikitaslezkin <ne.slezkin@gmail.com>
2021-02-09 14:23:50 +0300

    fix code on article

--- tutorial.py
+++ tutorial.py
@@ -5,6 +5,35 @@
 from circuit import Circuit
 from functions import *
 from math import ceil, log2
+from functions.sum import add_sum2, add_sum3, check_sum_circuit, add_sum5_suboptimal
+from functions.th import add_naive_thr2_circuit, add_efficient_thr2_circuit
+from circuit_search import CircuitFinder
+from circuit_improvement import improve_circuit
+
+
+# sum_2 circuit for checker
+def sum2(x1, x2):
+    w0 = x1 ^ x2
+    w1 = x1 * x2
+    return w0, w1
+
+
+# sum_3 circuit for checker
+def sum3(x1, x2, x3):
+    a = x1 ^ x2
+    b = x2 ^ x3
+    c = a | b
+    w0 = a ^ x3
+    w1 = c ^ w0
+    return w0, w1
+
+
+# Check correctness sum3 circuit
+from itertools import product
+
+for x1, x2, x3 in product(range(2), repeat=3):
+    w0, w1 = sum3(x1, x2, x3)
+    assert x1 + x2 + x3 == w0 + 2 * w1
 
 
 # Building a circuit for SUM5 out of SUM2 and SUM3 blocks
@@ -23,7 +52,48 @@ def sum_n(x):
     return [(sum(x) >> i) & 1 for i in range(ceil(log2(len(x) + 1)))]
 
 
-circuit = find_circuit(dimension=3, number_of_gates=5, function=sum_n)
+circuit_finder = CircuitFinder(dimension=3, number_of_gates=5,
+                               function=sum_n)
+circuit = circuit_finder.solve_cnf_formula()
 circuit.draw('sum3')
 
+
+# improve circuit function
+circuit = Circuit(input_labels=[f'x{i}' for i in range(1, 6)], gates={})
+circuit.outputs = add_sum5_suboptimal(circuit, circuit.input_labels)
+if isinstance(circuit.outputs, str):
+    circuit.outputs = [circuit.outputs]
+improved_circuit = improve_circuit(circuit, subcircuit_size=5,
+                                   connected=True)
+print(improved_circuit)
+improved_circuit.draw('sum5')
+
+
+# Construct circuit with fix gates and forbid wire
+def sum_n(x):
+    return [(sum(x) >> i) & 1 for i in range(ceil(log2(len(x) + 1)))]
+
+
+for n, size in ((3, 5), (4, 9), (5, 11)):
+    circuit_finder = CircuitFinder(dimension=n, number_of_gates=size,
+                                   function=sum_n)
+    circuit_finder.fix_gate(n, 0, 1, '0110')
+    for k in range(n - 2):
+        circuit_finder.fix_gate(n + k + 1, k + 2, n + k, '0110')
+    for i in range(1, n):
+        for j in range(n, n + size):
+            if i + n - 1 != j:
+                circuit_finder.forbid_wire(i, j)
+    circuit = circuit_finder.solve_cnf_formula(verbose=0)
+    circuit.draw(f'sum{n}')
+
+
+# Thr2 naive and efficient
+c = Circuit(input_labels=[f'x{i}' for i in range(1, 13)], gates={})
+c.outputs = add_naive_thr2_circuit(c, c.input_labels)
+c.draw('thr2naive')
+
+c = Circuit(input_labels=[f'x{i}' for i in range(1, 13)], gates={})
+c.outputs = add_efficient_thr2_circuit(c, c.input_labels, 3, 4)
+c.draw('thr2efficient')
 
