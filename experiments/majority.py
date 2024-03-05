78b27c4a6085c2ef5a7a492ab1d7e5ddc4c73ae2
Alexander S. Kulikov <alexander.s.kulikov@gmail.com>
2024-03-05 10:39:40 +0200

    Update majority generators

--- experiments/majority.py
+++ experiments/majority.py
@@ -8,18 +8,26 @@ def verify_majority_circuit(circuit):
         assert (table[value] == 1) == (sum(x) > n / 2)
 
 
-
-
-n = 11
+n = 9
 circuit = Circuit(input_labels=[f'x{i}' for i in range(1, n + 1)])
-w0, w1, w2, w3 = add_sum11_size36(circuit, circuit.input_labels)
-y1 = circuit.add_gate(w1, w2, '0001')
-y2 = circuit.add_gate(w3, y1, '0111')
+w0, w1, w2, w3 = add_sum9(circuit, circuit.input_labels)
+y1 = circuit.add_gate(w0, w1, '0111')
+y2 = circuit.add_gate(y1, w2, '0001')
+y2 = circuit.add_gate(y2, w3, '0111')
 circuit.outputs = [y2, ]
 verify_majority_circuit(circuit)
+circuit.save_to_file('maj09', extension='bench')
 
-n = 15
-circuit = Circuit(input_labels=[f'x{i}' for i in range(1, n + 1)])
-w0, w1, w2, w3 = add_sum15_size53(circuit, circuit.input_labels)
-circuit.outputs = [w3]
-verify_majority_circuit(circuit)
+# n = 11
+# circuit = Circuit(input_labels=[f'x{i}' for i in range(1, n + 1)])
+# w0, w1, w2, w3 = add_sum11_size36(circuit, circuit.input_labels)
+# y1 = circuit.add_gate(w1, w2, '0001')
+# y2 = circuit.add_gate(w3, y1, '0111')
+# circuit.outputs = [y2, ]
+# verify_majority_circuit(circuit)
+#
+# n = 15
+# circuit = Circuit(input_labels=[f'x{i}' for i in range(1, n + 1)])
+# w0, w1, w2, w3 = add_sum15_size53(circuit, circuit.input_labels)
+# circuit.outputs = [w3]
+# verify_majority_circuit(circuit)
