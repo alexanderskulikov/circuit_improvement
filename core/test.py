63dd25bb37375b4f051362c70182bcd46f2b8b02
Alexander S. Kulikov <alexander.s.kulikov@gmail.com>
2024-03-09 14:48:59 +0200

    Add a line that breaks circuit improvement

--- core/test.py
+++ core/test.py
@@ -5,5 +5,4 @@ circuit = Circuit(input_labels=[f'x{i}' for i in range(1, 8)])
 w0, w1, w2 = add_sum7(circuit, circuit.input_labels)
 circuit.outputs = [w2, ]
 better_circuit = improve_circuit(circuit)
-
-print(better_circuit.get_truth_tables())
+improve_circuit(better_circuit)
