e4c9be43237538e235b1768417245940374256ec
Alexander S. Kulikov <alexander.s.kulikov@gmail.com>
2024-03-20 12:54:47 +0200

    Improve sort10 circuit

--- experiments/sorting.py
+++ experiments/sorting.py
@@ -14,27 +14,27 @@ def verify_sorting_circuit(circuit):
 
 
 def find_block_for_sum_circuit(x):
-    s = x[0] + 2 * x[1] + 4 * x[2] + 8 * x[3] + 16 * x[4]
-    threshold = 16
+    s = x[0] + 2 * x[1] + 4 * x[2] + 8 * x[3]
+    threshold = 10
     if s > threshold:
         return ['*' for _ in range(threshold)]
     else:
         return [1 if s >= t else 0 for t in range(threshold, 0, -1)]
 
 
-
-# for size in range(22, 3, -1):
+# for size in range(16, 3, -1):
 #     finder = CircuitFinder(dimension=4, function=find_block_for_sum_circuit, number_of_gates=size,
-#                            input_labels=['z29', 'z40', 'z44', 'z47'])
+#                            input_labels=['z19', 'z27', 'z29', 'z30'])
 #     block = finder.solve_cnf_formula(verbose=True, solver='cadical195')
-#     block.save_to_file(f'block08_size{size}', extension='ckt')
+#     block.save_to_file(f'block10_size{size}', extension='ckt')
 
 ckt = Circuit()
-ckt.load_from_file('improved68', extension='ckt')
-# verify_sorting_circuit(ckt)
-# improve_circuit_iteratively(ckt, max_subcircuit_size=3)
-ckt.save_to_file('sort14_size68', extension='ckt')
-ckt.save_to_file('sort14_size68', extension='bench')
+ckt.load_from_file('improved44', extension='ckt')
+print(len(ckt.gates))
+verify_sorting_circuit(ckt)
+# improve_circuit_iteratively(ckt, max_subcircuit_size=5)
+ckt.save_to_file('sort10_size44', extension='ckt')
+ckt.save_to_file('sort10_size44', extension='bench')
 
 
 # ckt = Circuit(input_labels=[f'x{i}' for i in range(1, 15)])
