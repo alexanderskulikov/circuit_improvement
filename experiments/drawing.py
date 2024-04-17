0b8b626f1ba5e4454732660b4ae0824ef631e8a6
Alexander S. Kulikov <alexander.s.kulikov@gmail.com>
2024-04-17 11:39:54 +0300

    A simple wrapper for drawing

---
+++ experiments/drawing.py
@@ -0,0 +1,9 @@
+from core.circuit_improvement import *
+from functions.mult import *
+from core.circuit_search import *
+
+ckt = Circuit()
+ckt.load_from_file('ex43', extension='bench')
+ckt.draw('before')
+ckt.normalize()
+ckt.draw('after')
