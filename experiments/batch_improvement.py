c7119b44969577d0a276f3e3b36d4c486a843243
Alexander S. Kulikov <alexander.s.kulikov@gmail.com>
2024-04-22 08:13:36 +0300

    Update batch improvement script

--- experiments/batch_improvement.py
+++ experiments/batch_improvement.py
@@ -1,4 +1,3 @@
-from core.circuit_search import *
 from core.circuit_improvement import *
 from functions.sum import *
 from os import listdir
@@ -11,9 +10,5 @@ for file_name in sorted(listdir('./circuits/')):
     ckt.load_from_file(file_name[:-6], extension='bench')
     ckt.normalize()
     original_size = ckt.get_nof_true_binary_gates()
-    print(f'Processing {file_name} of size {original_size}')
-    ckt = improve_circuit_iteratively(ckt, max_subcircuit_size=2)
-    if ckt.get_nof_true_binary_gates() < original_size:
-        print('IMPROVED!!1')
-
-
+    print(f'Processing {file_name[:-6]} of size {original_size}')
+    improve_circuit_iteratively(ckt, file_name)
