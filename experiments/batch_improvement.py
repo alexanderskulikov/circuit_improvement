70ef5b695463a71c08a5ad14480e151e72f46833
Alexander S. Kulikov <alexander.s.kulikov@gmail.com>
2024-04-23 11:03:21 +0300

    Cosmetics

--- experiments/batch_improvement.py
+++ experiments/batch_improvement.py
@@ -1,6 +1,8 @@
 from core.circuit_improvement import *
-from functions.sum import *
+from datetime import datetime
 from os import listdir
+
+print('Start batch improvement. Time:', datetime.now())
 
 for file_name in sorted(listdir('./circuits/')):
     if file_name == '.images':
@@ -11,3 +13,5 @@ for file_name in sorted(listdir('./circuits/')):
     ckt.normalize()
     print(f'Processing {file_name[:-6]} of size {ckt.get_nof_true_binary_gates()}')
     improve_circuit_iteratively(ckt, file_name[:-6], max_subcircuit_size=7)
+
+print('Done! Time:', datetime.now())
