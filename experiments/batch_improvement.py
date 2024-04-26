3c958c5fd975075499db3062eebfb90d3ed3a578
Alexander S. Kulikov <alexander.s.kulikov@gmail.com>
2024-04-26 12:58:22 +0300

    Incorporate the basis into batch improvement

--- experiments/batch_improvement.py
+++ experiments/batch_improvement.py
@@ -2,6 +2,8 @@ from core.circuit_improvement import *
 from datetime import datetime
 from os import listdir
 
+basis = 'xaig'
+
 print(f'Start batch improvement ({datetime.now()})')
 
 for file_name in sorted(listdir('./circuits/')):
@@ -10,12 +12,12 @@ for file_name in sorted(listdir('./circuits/')):
 
     ckt = Circuit()
     ckt.load_from_file(file_name[:-6], extension='bench')
-    ckt.normalize()
+    ckt.normalize(basis)
 
     if len(ckt.gates) < ckt.get_nof_true_binary_gates():
         print(f'Skipping {file_name[:-6]} as it still contains unary gates')
     else:
         print(f'Processing {file_name[:-6]} of size {ckt.get_nof_true_binary_gates()} ({datetime.now()})')
-        improve_circuit_iteratively(ckt, file_name[:-6], basis='xaig')
+        improve_circuit_iteratively(ckt, file_name[:-6], basis=basis, time_limit=10)
 
 print(f'Done! ({datetime.now()})')
