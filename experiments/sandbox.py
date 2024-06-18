61dd7e1266946dde5fa930c4346df5dd5a1180d4
Alexander S. Kulikov <alexander.s.kulikov@gmail.com>
2024-06-18 17:28:15 +0200

    Add a file for experimenting with SUM in the AIG basis

--- experiments/sandbox.py
+++ experiments/sandbox.py
@@ -1,14 +1,12 @@
 from core.circuit_improvement import *
 from functions.mult import *
 from functions.th import *
+from functions.sum import *
 from core.circuit_search import *
 from datetime import datetime
+from core.circuit_improvement import *
 
-finder = CircuitFinder(
-    dimension=4,
-    number_of_gates=11,
-    output_truth_tables=['0000000000000001', '0001011101111111', '0110100110010110'],
-    forbidden_operations=['0110', '1001']
-)
-ckt = finder.solve_cnf_formula()
-print(ckt)
+ckt = Circuit(input_labels=[f'x{i}' for i in range(7)])
+ckt.outputs = add_sum(ckt, ckt.input_labels, basis='aig')
+print(ckt.get_nof_true_binary_gates())
+improve_circuit_iteratively(ckt, file_name='sum7_aig', basis='aig', speed=10)
