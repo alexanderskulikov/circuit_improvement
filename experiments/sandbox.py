96b807f23c8142f9855acdf68b79bcc1050a1c7e
Alexander S. Kulikov <alexander.s.kulikov@gmail.com>
2024-11-02 20:27:12 +0100

    Update the sandbox file so that it searches for a circuit

--- experiments/sandbox.py
+++ experiments/sandbox.py
@@ -6,7 +6,7 @@ from core.circuit_search import *
 from datetime import datetime
 from core.circuit_improvement import *
 
-ckt = Circuit(input_labels=[f'x{i}' for i in range(7)])
-ckt.outputs = add_sum(ckt, ckt.input_labels, basis='aig')
-print(ckt.get_nof_true_binary_gates())
-improve_circuit_iteratively(ckt, file_name='sum7_aig', basis='aig', speed=10)
+finder = CircuitFinder(dimension=2, output_truth_tables=['0110', ], basis='aig', number_of_gates=3)
+ckt = finder.solve_cnf_formula()
+print(ckt)
+
