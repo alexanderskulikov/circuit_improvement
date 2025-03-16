1096da129c18cd76e8d57557ffdd7a80163717f7
Alexander S. Kulikov <alexander.s.kulikov@gmail.com>
2025-03-16 09:37:03 +0200

    Add a wrapper for iterative circuit improvement

--- experiments/iterative_improvement.py
+++ experiments/iterative_improvement.py
@@ -1,17 +1,12 @@
-from core.circuit_improvement import improve_circuit
-from functions.sum import *
-from core.circuit_search import *
+from core.circuit_improvement import improve_circuit_iteratively
+from core.circuit import *
 
+
+# put your circuit (say, ex01.bench) to the eperiments/curcuits folder, then load it as follows
 circuit = Circuit()
-circuit.load_from_file('ex98', extension='bench')
+circuit.load_from_file('ex01', extension='bench')
+circuit.normalize(basis='aig')
 
-was_improved = True
-while was_improved:
-    was_improved = False
+# you can adjust the speed in the call below (1--18)
+improve_circuit_iteratively(circuit, file_name='tmp', basis='aig', save_circuits=True, speed=10)
 
-    for subcircuit_size in range(2, 6):
-        better_circuit = improve_circuit(circuit, subcircuit_size=subcircuit_size)
-        if isinstance(better_circuit, Circuit):
-            was_improved = True
-            circuit = better_circuit
-            break
