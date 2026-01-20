8f3067d0c2052211219a563dca994ada220c8788
Alexander S. Kulikov <alexander.s.kulikov@gmail.com>
2026-01-20 15:35:12 +0200

    Typo

--- experiments/mod3/two_circuits_differ_in_one_gate.py
+++ experiments/mod3/two_circuits_differ_in_one_gate.py
@@ -1,21 +1,37 @@
 from itertools import product
 
-def mod3_5_2(x1, x2, x3, x4, x5):
+
+def mod3_5_2_first(x1, x2, x3, x4, x5):
     g5 = x1 ^ x2
     g6 = x3 ^ g5
     g7 = x4 ^ x5
-    g8 = g6 ^ g6
+    g8 = g6 ^ g7
     g9 = x3 ^ x4
     g10 = g7 | g9
     g11 = x1 * x2
     g12 = g10 ^ g11
     g13 = g8 * g10
     g14 = g12 * (1 - g13)
-    print(g5, g6, g7, g8, g9, g10, g11, g12, g13, g14)
     return g14
+
+
+def mod3_5_2_second(x1, x2, x3, x4, x5):
+    g5 = x1 | x2
+    g6 = x3 ^ g5
+    g7 = x4 ^ x5
+    g8 = g6 ^ g7
+    g9 = x3 ^ x4
+    g10 = g7 | g9
+    g11 = x1 * x2
+    g12 = g10 ^ g11
+    g13 = g8 * g10
+    g14 = g12 * (1 - g13)
+    return g14
+
 
 for x in product((0, 1), repeat=5):
     if sum(x) % 3 == 2:
-        assert mod3_5_2(*x) == 1
+        assert mod3_5_2_first(*x) == 1 and mod3_5_2_second(*x) == 1
     else:
-        assert mod3_5_2(*x) == 0
+        assert mod3_5_2_first(*x) == 0 and mod3_5_2_second(*x) == 0
+        
