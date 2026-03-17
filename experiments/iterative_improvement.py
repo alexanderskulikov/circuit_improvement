1690b93f52601196b9d3596662b146c803abe228
Alexander S. Kulikov <alexander.s.kulikov@gmail.com>
2026-03-17 19:46:55 +0200

    Cosmetics

--- experiments/iterative_improvement.py
+++ experiments/iterative_improvement.py
@@ -3,23 +3,33 @@ from core.circuit import *
 import os
 import re
 
+# with open('processed-bench-names.txt', 'r+') as processed_benches_file:
+#     processed_bench_names = [line.rstrip() for line in processed_benches_file]
 
-folder = 'circuits'
-for file_name in sorted(os.listdir(folder)):
+
+
+
+exit()
+
+
+input_folder, output_folder, max_size = 'pareto', 'pareto-improved', 100
+
+for file_name in sorted(os.listdir(input_folder)):
     if not file_name.endswith('.bench'):
         continue
 
+    fields = re.split(r'[_, .]+', file_name)
+    bench_name, depth, size = fields[0], int(fields[1]), int(fields[2])
+
     print('Processing', file_name)
 
-    fields = re.split(r'[_, .]+', file_name)
-    depth, size = int(fields[1]), int(fields[2])
-
-    if size > 2000:
+    if size > max_size:
+        print('  skip: the size is too large')
         continue
 
     try:
         circuit = Circuit()
-        circuit.load_from_file(path=folder + '/' + file_name)
+        circuit.load_from_file(path=input_folder + '/' + file_name)
         circuit.normalize(basis='aig')
 
         improve_circuit_iteratively(
@@ -28,9 +38,9 @@ for file_name in sorted(os.listdir(folder)):
             basis='aig',
             save_circuits=True,
             speed='easy',
-            global_time_limit= 60 * 60 * 17,
-            # global_time_limit=60 * 60 * 24 * 2,
-            keep_depth=False
+            global_time_limit= 60 * 2,
+            keep_depth=False,
+            output_folder=output_folder
         )
     except Exception as err:
         print(f'    \033[91mUnexpected {err=}, {type(err)=}\033[0m')
