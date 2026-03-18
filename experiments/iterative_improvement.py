8995910db3011a91b79650eee2f6600ee2ce7ae5
Alexander S. Kulikov <alexander.s.kulikov@gmail.com>
2026-03-18 12:16:02 +0200

    Add logging for the benchmarks that has already been processed

--- experiments/iterative_improvement.py
+++ experiments/iterative_improvement.py
@@ -3,25 +3,29 @@ from core.circuit import *
 import os
 import re
 
-# with open('processed-bench-names.txt', 'r+') as processed_benches_file:
-#     processed_bench_names = [line.rstrip() for line in processed_benches_file]
+processed_benches_file_name = 'processed-bench-names.txt'
+input_folder, output_folder, max_size = '/Users/Alexander.Kulikov/Desktop/pareto', '/Users/Alexander.Kulikov/Desktop/pareto-improved', 500
 
 
+if not os.path.exists(processed_benches_file_name):
+    with open(processed_benches_file_name, 'w'):
+        pass
 
-
-exit()
-
-
-input_folder, output_folder, max_size = 'pareto', 'pareto-improved', 100
+with open(processed_benches_file_name, 'r') as processed_benches_file:
+    processed_bench_names = [line.rstrip() for line in processed_benches_file]
 
 for file_name in sorted(os.listdir(input_folder)):
     if not file_name.endswith('.bench'):
         continue
 
+    print('Processing', file_name)
+
+    if file_name in processed_bench_names:
+        print('  skip: has been processed already')
+        continue
+
     fields = re.split(r'[_, .]+', file_name)
     bench_name, depth, size = fields[0], int(fields[1]), int(fields[2])
-
-    print('Processing', file_name)
 
     if size > max_size:
         print('  skip: the size is too large')
@@ -38,10 +42,13 @@ for file_name in sorted(os.listdir(input_folder)):
             basis='aig',
             save_circuits=True,
             speed='easy',
-            global_time_limit= 60 * 2,
+            global_time_limit=60,
             keep_depth=False,
             output_folder=output_folder
         )
+
+        with open(processed_benches_file_name, 'a') as processed_benches_file:
+            processed_benches_file.write(file_name + '\n')
+
     except Exception as err:
         print(f'    \033[91mUnexpected {err=}, {type(err)=}\033[0m')
-
