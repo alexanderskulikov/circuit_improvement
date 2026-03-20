938815af124a45a018f3e4fbacb7180a4e3fd1ce
Alexander S. Kulikov <alexander.s.kulikov@gmail.com>
2026-03-20 10:14:59 +0200

    Add a timelimit for the whole front

--- experiments/iterative_improvement.py
+++ experiments/iterative_improvement.py
@@ -4,7 +4,10 @@ import os
 import re
 
 processed_benches_file_name = 'processed-bench-names.txt'
-input_folder, output_folder, max_size = 'circuits', 'circuits/improved', 4000
+# input_folder, output_folder, max_size = 'circuits', 'circuits/improved', 4000
+input_folder, output_folder, max_size = '/Users/Alexander.Kulikov/Desktop/pareto', '/Users/Alexander.Kulikov/Desktop/pareto-improved', 2000
+# total_time = 60 * 60 * 24 * 3  # 3 days
+total_time = 60 * 60  # one hour
 
 
 if not os.path.exists(processed_benches_file_name):
@@ -14,22 +17,39 @@ if not os.path.exists(processed_benches_file_name):
 with open(processed_benches_file_name, 'r') as processed_benches_file:
     processed_bench_names = [line.rstrip() for line in processed_benches_file]
 
+files_to_be_processed = []
+nof_too_big, nof_processed_already = 0, 0
+
 for file_name in sorted(os.listdir(input_folder)):
     if not file_name.endswith('.bench'):
         continue
 
-    print('Processing', file_name)
-
     if file_name in processed_bench_names:
-        print('  skip: has been processed already')
+        nof_processed_already += 1
         continue
 
     fields = re.split(r'[_, .]+', file_name)
-    bench_name, depth, size = fields[0], int(fields[1]), int(fields[2])
+    size = int(fields[2])
 
     if size > max_size:
-        print('  skip: the size is too large')
+        nof_too_big += 1
         continue
+
+    files_to_be_processed.append(file_name)
+
+single_benchmark_timelimit = total_time / max(1, len(files_to_be_processed))
+
+print('Start improving Pareto front')
+print(f'Skipping {nof_too_big} benches as they are too large and skipping {nof_processed_already} benches as they have been processed already')
+print(f'The total time given is {total_time} sec (or {total_time / 3600:.2f} hours, or {total_time / 3600 / 24:.2f} days),')
+print(f'so each benchmark will be given {single_benchmark_timelimit:.2f} sec (or {single_benchmark_timelimit / 3600:.2f} hours)\n')
+
+
+for file_name in files_to_be_processed:
+    print('Processing', file_name)
+
+    fields = re.split(r'[_, .]+', file_name)
+    bench_name = fields[0]
 
     try:
         circuit = Circuit()
@@ -42,7 +62,7 @@ for file_name in sorted(os.listdir(input_folder)):
             basis='aig',
             save_circuits=True,
             speed='easy',
-            global_time_limit=60 * 60 * 40,
+            global_time_limit=single_benchmark_timelimit,
             keep_depth=False,
             output_folder=output_folder
         )
