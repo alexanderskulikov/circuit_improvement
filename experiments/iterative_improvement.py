bb953f6d3c5b16f4f998ce23a8d102b1489e5785
Alexander S. Kulikov <alexander.s.kulikov@gmail.com>
2026-03-21 10:21:37 +0200

    Cosmetics

--- experiments/iterative_improvement.py
+++ experiments/iterative_improvement.py
@@ -1,12 +1,16 @@
 from core.circuit_improvement import improve_circuit_iteratively
 from core.circuit import *
+from datetime import datetime, timedelta
+
 import os
 import re
 
 processed_benches_file_name = 'processed-bench-names.txt'
-input_folder, output_folder, max_size = '/Users/Alexander.Kulikov/Desktop/pareto', '/Users/Alexander.Kulikov/Desktop/pareto-improved', 2000
+input_folder, output_folder = '/Users/Alexander.Kulikov/Desktop/pareto', '/Users/Alexander.Kulikov/Desktop/pareto-improved'
+max_size = 1000
+
 # total_time = 60 * 60 * 24 * 3  # 3 days
-total_time = 60 * 60 * 9  # nine hours
+total_time = 60 * 45  # nine hours
 
 
 if not os.path.exists(processed_benches_file_name):
@@ -36,14 +40,13 @@ for file_name in sorted(os.listdir(input_folder)):
 
     files_to_be_processed.append(file_name)
 
-single_benchmark_timelimit = total_time / max(1, len(files_to_be_processed))
+single_benchmark_timelimit = total_time // max(1, len(files_to_be_processed))
 
-print('Start improving Pareto front')
-print(f'  Skipping {nof_too_big} benches as they are too large and skipping {nof_processed_already} benches as they have been processed already')
-print(f'  Processing {len(files_to_be_processed)} benches')
-print(f'  The total time given is {total_time} sec (or {total_time / 3600:.1f} hours, or {total_time / 3600 / 24:.1f} days),'
-      f' so each benchmark will be given {single_benchmark_timelimit:.1f} sec (or {single_benchmark_timelimit / 3600:.1f} hours)\n')
-
+print(f'''
+Start improving Pareto front
+Processing {len(files_to_be_processed)} benches, giving each one {single_benchmark_timelimit} sec (or {single_benchmark_timelimit / 3600:.1f} hours), and skipping {nof_too_big} large benches and {nof_processed_already} already processed benches
+ETA: {(datetime.now() + timedelta(seconds=total_time)).strftime("%Y-%m-%d %H:%M:%S")}
+''')
 
 for file_name in files_to_be_processed:
     print('\nProcessing', file_name)
